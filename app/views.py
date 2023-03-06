f6acdb24591deac368668d26913846d23cf99ca1
Bharathsg4 <bharathsg119@gmail.com>
2023-03-06 21:58:07 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def settipalli(request):
+    return HttpResponse('<h1>Bharath Simha Reddy!!</h1>')
+def ram(request):
+    return HttpResponse('<h1>KONIDELA RAM CHARAN!!</h1>')
